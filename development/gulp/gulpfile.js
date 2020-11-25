8fd0c0dfa2945af52bf129ecc70aadcf77158ace
= <=>
2020-11-25 10:24:38 -0300

    gulp refactor

--- development/gulp/gulpfile.js
+++ development/gulp/gulpfile.js
@@ -1,3 +1,12 @@
 const gulp = require('gulp'); //npm install gulp-cli --global --save-dev // npm install gulp --save-dev //http://gulpjs.com
 const requireDir = require('require-dir'); //npm install require-dir --save-dev // https://www.npmjs.com/package/require-dir
-const tasks = requireDir('./task', {recurse: true});
+const tasks = requireDir('./wf-task/', {
+    recurse: true
+});
+
+gulp.task('default',
+    gulp.series(
+        'watch',
+        'webserver',
+    )
+);
