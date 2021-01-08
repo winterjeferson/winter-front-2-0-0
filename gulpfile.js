a34cb50a55324b6871612fcac582ba24e689b96b
= <=>
2021-01-08 17:14:55 -0300

    folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 const gulp = require('gulp'); //npm install gulp-cli --global --save-dev // npm install gulp --save-dev //http://gulpjs.com
 const requireDir = require('require-dir'); //npm install require-dir --save-dev // https://www.npmjs.com/package/require-dir
-const tasks = requireDir('./wf-task/', {
+const gulpWfTask = requireDir('./gulp-wf-task/', {
     recurse: true
 });
 
-gulp.task('default',
+gulp.gulpWfTask('default',
     gulp.series(
         'watch',
         'initialize',
